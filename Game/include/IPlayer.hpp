49c7e4e01f07114a99af60e26a190d41a9382e44
evgeniy-kozev <legionsnov@rambler.ru>
2023-04-02 20:01:09 +0300

    Добавил интерфейс для отображения игрока

---
+++ Game/include/IPlayer.hpp
@@ -0,0 +1,13 @@
+#ifndef IPLAYER_HPP
+#define IPLAYER_HPP
+
+class IPlayer {
+public:
+
+  virtual ~IPlayer() = default;
+
+  virtual void ShowPlayer(int x,
+                          int y) = 0;
+};
+
+#endif // IPLAYER_HPP
