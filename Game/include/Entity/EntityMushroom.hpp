bc749b0596ab0a53d7bc29d0127f8010b8ca5b6c
Dmitry Morozov <9984398@gmail.com>
2023-04-07 20:32:12 +0300

    -> EntityMushroom : Refactoring and fix

--- Game/include/Entity/EntityMushroom.hpp
+++ Game/include/Entity/EntityMushroom.hpp
@@ -8,19 +8,42 @@
 #include "GL/glew.h"
 
 #include "Entity.hpp"
+#include "EventSystem.hpp"
+#include "Texture.hpp"
 
 class EntityMushroom : public Entity {
  public:
 
-  EntityMushroom(b2World &world, glm::vec2 coord, glm::vec2 size) {  }
+  EntityMushroom(EventSystem &event_handler, b2World &world, glm::vec2 position, glm::vec2 scale);
 
-  ~EntityMushroom() override = default;
+  ~EntityMushroom() override;
 
   void onUpdate(float delta) override;
   void onRender() override;
   void onCollision(std::shared_ptr<IEntity> collision_entity) override;
 
  private:
+  void RunAnimation(float ts);
+  void CountToDie(float ts);
+
+ private:
+  bool isAlive = true;
+  bool isDie = false; // For delay dead texture
+
+  float time_to_disappear_ = 1.f;
+  float speed_ = 2.f;
+  bool faceRight_ = false;
+
+  Texture::TexturePtr current_texture;
+  Texture::TexturePtr run_texture[2];
+  Texture::TexturePtr dead_texture;
+
+  // Animations
+  int texture_index = 0;
+  float animation_speed = 0.1f;
+  float animation_time_btw = animation_speed;
+
+  b2Body *mp_Body;
 
 };
 
--- Game/src/Entity/EntityMushroom.cpp
+++ Game/src/Entity/EntityMushroom.cpp
@@ -2,18 +2,111 @@
 // Created by Dmitry Morozov on 25/3/23.
 //
 
+#include "glm/glm.hpp"
+
 #include "Entity/EntityMushroom.hpp"
+#include "Graphics/Render.hpp"
+
+EntityMushroom::EntityMushroom(EventSystem &event_handler, b2World &world, glm::vec2 position, glm::vec2 scale) {
+
+  vPosition = position;
+  vRotation = 0;
+  vScale = scale;
+
+  event_handler.addEventHandler(*this);
+
+  run_texture[0] = Texture::GetTexture("mushroom-run-0");
+  run_texture[1] = Texture::GetTexture("mushroom-run-1");
+  dead_texture = Texture::GetTexture("mushroom-dead");
+
+  current_texture = run_texture[0];
+
+  vTag = "mushroom";
+
+  b2BodyDef b_def;
+
+  b_def.userData.pointer = reinterpret_cast<uintptr_t>(this);
+  b_def.type = b2_dynamicBody;
+  b_def.fixedRotation = true;
+
+  b_def.position.Set(vPosition.x/pixelToM_, vPosition.y/pixelToM_);
+
+  b_def.angle = glm::radians(vRotation);
+
+  this->mp_Body = world.CreateBody(&b_def);
+
+  b2PolygonShape box_shape;
+
+  box_shape.SetAsBox(vScale.x/2.f/pixelToM_, vScale.y/2.f/pixelToM_);
+
+  b2FixtureDef fixDef;
+
+  fixDef.shape = &box_shape;
+  fixDef.density = 1;
+  fixDef.friction = 0.3f;
+
+  this->mp_Body->CreateFixture(&fixDef);
+
+}
 
 void EntityMushroom::onUpdate(float delta) {
-  Entity::onUpdate(delta);
+
+  if (!isAlive) {
+	return;
+  }
+
+  vPosition = {mp_Body->GetPosition().x*150, mp_Body->GetPosition().y*150};
+  vRotation = glm::degrees(mp_Body->GetAngle());
+
+  b2Vec2 vel = mp_Body->GetLinearVelocity();
+
+  if (faceRight_) {
+
+	vel.x = speed_;
+
+  } else {
+
+	vel.x = -speed_;
+
+  }
+
+  mp_Body->SetLinearVelocity(vel);
+
+  if (isDie) {
+
+	CountToDie(delta);
+
+  }
+
+  if (current_texture!=dead_texture) {
+
+	RunAnimation(delta);
+
+  }
 }
 
 void EntityMushroom::onRender() {
-  Entity::onRender();
+
+  if (!isAlive) {
+
+	return;
+  }
+
+  Render::DrawTexture(vPosition, vRotation, vScale, current_texture);
+
 }
 
 void EntityMushroom::onCollision(std::shared_ptr<IEntity> collision_entity) {
   Entity::onCollision(collision_entity);
+}
+
+EntityMushroom::~EntityMushroom() {
+
+  run_texture[0].reset();
+  run_texture[1].reset();
+  current_texture.reset();
+  dead_texture.reset();
+
 }
 
 
