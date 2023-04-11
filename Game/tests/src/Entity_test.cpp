7046a143e7c0ab3bcf0955047560c5f9bdd64eb7
Dmitry Morozov <9984398@gmail.com>
2023-04-11 17:20:11 +0300

    Void test commit

--- Game/tests/src/Entity_test.cpp
+++ Game/tests/src/Entity_test.cpp
@@ -12,8 +12,18 @@ TEST(EntityTest, Constructor) {
 
   EXPECT_EQ(e.GetTag(), "default");
   EXPECT_EQ(e.GetPosition(), glm::vec2(0.0f, 0.0f));
-  EXPECT_EQ(e.GetRotation(), 0.0f );
+  EXPECT_EQ(e.GetRotation(), 0.0f);
   EXPECT_EQ(e.GetScale(), glm::vec2(1.0f, 1.0f));
+
+}
+
+TEST(EntityTest, SetTag) {
+  Entity e;
+
+  EXPECT_EQ(e.GetTag(), "default");
+  std::string tag = "new tag";
+  e.SetTag(tag);
+  EXPECT_EQ(e.GetTag(), tag);
 
 }
 
